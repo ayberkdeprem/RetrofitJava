8f59ae2ca693fb79481e1cbef32d1e610e59dd01
Ayberk <ayberkdeprem@gmail.com>
2022-08-30 15:15:56 +0300

    init commit

--- app/src/main/java/com/deprems/retrofitjava/view/MainActivity.java
+++ app/src/main/java/com/deprems/retrofitjava/view/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //https://api.nomics.com/v1/currencies/ticker?key=48a740d54f1124652655657fd05add4c702fd4d1
+
+
+
+
     }
 }
